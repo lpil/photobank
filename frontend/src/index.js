ec24d26439f8d8a57ef24ae7b340f6678cd290b4
Louis Pilfold <louis@lpil.uk>
2017-07-29 19:52:00 +0100

    Generate Elm Queries module

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,8 @@
 const Elm = require("./Main.elm");
-const flags = {};
+
+const flags = {
+  // TODO: Make this configurable between environments
+  backendEndpoint: "localhost:4000"
+};
+
 const app = Elm.Main.embed(document.getElementById("js-app"), flags);
